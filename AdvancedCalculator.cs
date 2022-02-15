11058ee080abb52f8ccd192371ef90603b80bb22
Roshan Dangal <rdangal@redventures.com>
2022-02-15 07:43:39 -0700

    initial commit

---
+++ AdvancedCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    public class AdvancedCalculator
+    {
+        public decimal _result { get; set; } = 0;
+        public void Add(decimal input)
+        {
+            _result = _result + input;
+        }
+        public void Clear()
+        {
+            _result = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+
+var calculator = new AdvancedCalculator();
+
+Console.WriteLine("Please Enter + to add and . to clear");
+
+bool shouldContinue = true;
+
+do
+{
+    Console.WriteLine("Please insert operations, + , - , .");
+    char action = Console.ReadKey().KeyChar;
+    Console.Clear();
+    Console.WriteLine("Your TotalNumber is: " + calculator._result);
+
+    if (action == '.')
+    {
+        calculator.Clear();
+        //shouldContinue = false;
+    }
+    else
+    {
+        Console.WriteLine("Please Enter Number to add");
+        decimal numberToAdd;
+        string str = Console.ReadLine();
+        decimal.TryParse(str, out numberToAdd);
+        calculator.Add(numberToAdd);
+        Console.WriteLine("Your Number is: " + calculator._result);
+    }
+
+} while (shouldContinue);
+
